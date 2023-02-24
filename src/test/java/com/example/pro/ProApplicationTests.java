e15284e59b141911a01b33ff202e9f0717f2d950
chhong <chhong@donga.com>
2023-02-24 23:12:19 +0900

    02.23

--- src/test/java/com/example/pro/ProApplicationTests.java
+++ src/test/java/com/example/pro/ProApplicationTests.java
@@ -1,14 +1,20 @@
 package com.example.pro;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 @EnableBatchProcessing
 @SpringBootTest
-class ProApplicationTests {
+class ProApplicationTests implements CommandLineRunner {
 
     @Test
     void contextLoads() {
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        JobParameters jobParameters;
+    }
 }
