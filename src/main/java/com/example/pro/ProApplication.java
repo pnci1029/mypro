bcbc030b3f3746c2d00009a298d9fd9f86f3ba87
chhong <chhong@donga.com>
2023-03-05 12:58:58 +0900

    03.05 actuator 끝

--- src/main/java/com/example/pro/ProApplication.java
+++ src/main/java/com/example/pro/ProApplication.java
@@ -1,7 +1,9 @@
 package com.example.pro;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProApplication {
@@ -10,4 +12,11 @@ public class ProApplication {
         SpringApplication.run(ProApplication.class, args);
     }
 
+    /**
+     * 스프링 액츄에이터 httpExchange 구현체 빈 등록
+     */
+    @Bean
+    public InMemoryHttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 }
--- src/main/java/com/example/pro/controller/ArticleController.java
+++ src/main/java/com/example/pro/controller/ArticleController.java
@@ -2,18 +2,24 @@ package com.example.pro.controller;
 
 import com.example.pro.sertvice.ArticleService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController @RequestMapping("/article")
-@RequiredArgsConstructor
+@RequiredArgsConstructor @Slf4j
 public class ArticleController {
 
     private final ArticleService articleService;
     @GetMapping("/articles")
     public String getArticles() {
+        log.trace("log Trace");
+        log.debug("log debug");
+        log.info("log info");
+        log.warn("log warn") ;
+        log.error("log error"); ;
         return "전체 조회";
     }
 
