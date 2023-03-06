6eca467edb8cf4769661a31b9bac9f04cb098e5c
chhong <chhong@donga.com>
2023-03-06 19:41:17 +0900

    03.06 테스트코드

---
+++ src/main/java/com/example/pro/service/ArticleService.java
@@ -0,0 +1,45 @@
+package com.example.pro.service;
+
+import com.example.pro.domain.entity.Article;
+import com.example.pro.domain.entity.Member;
+import com.example.pro.domain.repository.ArticleRepository;
+import com.example.pro.domain.repository.MemberRepository;
+import com.example.pro.dto.article.articleRequestDto.ArticleRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Service @RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+    private final MemberRepository memberRepository;
+    public Article postArticle(@Valid ArticleRequestDto articleRequestDto) {
+//        Member member = null;
+//        if (memberRepository.findById(memberId).isPresent()) {
+//            member = memberRepository.findById(memberId).get();
+//        } else {
+//            throw new NullPointerException("nullPoint Error");
+//        }
+        Article data = Article.builder()
+                .title(articleRequestDto.getTitle())
+                .content(articleRequestDto.getContent())
+                .sq(articleRequestDto.getSq())
+                .bc(articleRequestDto.getBc())
+                .dl(articleRequestDto.getDl())
+//                .member(member)
+                .build();
+        return articleRepository.save(data);
+
+    }
+
+    public List<Article> getAllArticles() {
+        return articleRepository.findAll();
+    }
+
+    public void patchArticle(@Valid ArticleRequestDto articleRequestDto) {
+
+    }
+}
--- src/test/java/com/example/pro/controller/ArticleControllerTest.java
+++ src/test/java/com/example/pro/controller/ArticleControllerTest.java
@@ -1,35 +1,163 @@
 package com.example.pro.controller;
 
+import com.example.pro.domain.entity.Article;
+import com.example.pro.domain.entity.Member;
+import com.example.pro.domain.repository.ArticleRepository;
+import com.example.pro.domain.repository.MemberRepository;
+import com.example.pro.dto.article.articleRequestDto.ArticleRequestDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-@SpringBootTest @RestController
+@AutoConfigureMockMvc
+@SpringBootTest @Transactional
 public class ArticleControllerTest {
-    Mock mock;
-
-    ArticleController articleController;
-
+    @Autowired
+    MockMvc mockMvc;
+    ObjectMapper om = new ObjectMapper();
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    ArticleRepository articleRepository;
+    @BeforeEach
+    void 회원_생성() {
+        memberRepository.deleteAll();
+        Member member1 = new Member("user1", 20);
+        Member member2 = new Member("이이", 15);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+    }
     @Test
-    void 게시글_테스트1() {
-        ArticleController article = mock(ArticleController.class);
-        when(article.getArticles()).thenReturn("전체 조회1");
-        String result = article.getArticles();
-        Assertions.assertEquals(result,"전체 조회1");
+    void 개시글_조회() {
+        Article article = new Article("title","content",100,100,100);
+
+        articleRepository.save(article);
+
+        try {
+            mockMvc.perform(
+                            get("/article/articles")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                    )
+                    .andDo(print())
+                    .andExpect(content().string(om.writeValueAsString(articleRepository.findAll())))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            throw new IllegalStateException("error");
+        }
     }
 
     @Test
-    void 게시글_테스트2() {
+    void 게시글_작성_예외처리_NOT_NULL_실패_테스트() {
+        ArticleRequestDto target = ArticleRequestDto.builder()
+                .title(null)
+                .content("content")
+                .dl(100)
+                .sq(20)
+                .bc(50)
+                .build();
+        try {
+            this.mockMvc.perform(post("/article/post")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(om.writeValueAsString(target)))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(value -> Assertions.assertTrue(value.getResolvedException() instanceof MethodArgumentNotValidException));
+        } catch (Exception e) {
+            throw new IllegalStateException("error");
+        }
+    }
+
+    @Test
+    void 게시글_작성_예외처리_MIN_테스트() {
+        ArticleRequestDto target = ArticleRequestDto.builder()
+                .title("title")
+                .content("content")
+                .dl(100)
+                .sq(20)
+                .bc(50)
+                .build();
+        try {
+            this.mockMvc.perform(post("/article/post")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(om.writeValueAsString(target)))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(value -> Assertions.assertTrue(value.getResolvedException() instanceof MethodArgumentNotValidException));
+        } catch (Exception e) {
+            throw new IllegalStateException("error");
+        }
+    }
+
+    @Test
+    void 게시글_작성_성공_테스트() throws Exception {
+        ArticleRequestDto target = ArticleRequestDto.builder()
+                .title("title")
+                .content("content")
+                .dl(100)
+                .sq(50)
+                .bc(120)
+                .build();
+
+        this.mockMvc.perform(post("/article/post")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(om.writeValueAsString(target)))
+                .andDo(print())
+                // 최신순 정렬 후 가장 최신 값 조회 비교
+                .andExpect(content().string(om.writeValueAsString(articleRepository.findAll(Sort.by(Sort.Direction.DESC, "id")).get(0))))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void 게시글_작성_테스트() {
+        ArticleRequestDto target = ArticleRequestDto.builder()
+                .content("content")
+//                .title("title")
+                .bc(10)
+                .sq(150)
+                .dl(200)
+                .build();
         ArticleController article = mock(ArticleController.class);
-        when(article.articlePost()).thenReturn("포스트");
-        String result = article.articlePost();
-        Assertions.assertEquals("포스트",result);
+//        when(article.articlePost(target)).thenReturn(articleRepository.);
+//        String result = article.articlePost(target);
+//        System.out.println(result);
+
+        try {
+            this.mockMvc.perform(post("/article/post")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(om.writeValueAsString(target)))
+                    .andDo(print())
+                    .andExpect(value -> Assertions.assertTrue(value.getResolvedException() instanceof MethodArgumentNotValidException));
+        } catch (Exception e) {
+            throw new IllegalStateException("error");
+        }
+    }
+
+    @Test
+    void 게시글_저장() {
+
     }
 
 }
