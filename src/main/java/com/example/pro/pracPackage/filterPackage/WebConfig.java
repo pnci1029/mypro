37d21c4c8e9bd826d13d5ec5da93c93fc19c71d9
chhong <chhong@donga.com>
2023-03-16 19:47:30 +0900

    03.16 필터 추가

--- src/main/java/com/example/pro/pracPackage/filterPackage/WebConfig.java
+++ src/main/java/com/example/pro/pracPackage/filterPackage/WebConfig.java
@@ -1,7 +1,25 @@
 package com.example.pro.pracPackage.filterPackage;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+
+@Configuration @RequiredArgsConstructor
 public class WebConfig {
+
+
+    @Bean
+    public FilterRegistrationBean loginCheck() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
 }
