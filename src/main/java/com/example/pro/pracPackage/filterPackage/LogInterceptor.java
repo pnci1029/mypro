a41fb824e8c2e6062c8f92f2a25a0c28d0113225
chhong <chhong@donga.com>
2023-04-01 16:12:01 +0900

    04.01 Interceptor

---
+++ src/main/java/com/example/pro/pracPackage/filterPackage/LogInterceptor.java
@@ -0,0 +1,29 @@
+package com.example.pro.pracPackage.filterPackage;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        request.setAttribute("logId",uuid);
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        request.getAttribute("logId");
+    }
+}
