3972431fbd239c63af2fef78f198ef70a4f1bcf7
chhong <chhong@donga.com>
2023-06-05 20:24:34 +0900

    06_05 new pro

---
+++ springDbPrac/src/main/java/com/example/springdbprac/SpringDbPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdbprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDbPracApplication.class, args);
+    }
+
+}
---
+++ springDbPrac/src/test/java/com/example/springdbprac/SpringDbPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdbprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
