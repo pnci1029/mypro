95a3fbc0d68f3706f7382d0a6e5ddd8f90211f54
chhong <chhong@donga.com>
2023-06-12 23:26:48 +0900

    06_12 순수 JDBC 연결

---
+++ springDbPrac/src/main/java/com/example/springdbprac/domain/Member.java
@@ -0,0 +1,12 @@
+package com.example.springdbprac.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Member {
+    private String memberId;
+    private int money;
+
+}
