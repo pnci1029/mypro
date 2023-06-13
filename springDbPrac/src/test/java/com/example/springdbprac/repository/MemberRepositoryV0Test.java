f40ca0da03a652309ee130fa1a8a79d4424a8c6d
chhong <chhong@donga.com>
2023-06-13 22:45:50 +0900

    06_13 순수 JDBC CRUD

---
+++ springDbPrac/src/test/java/com/example/springdbprac/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,38 @@
+package com.example.springdbprac.repository;
+
+import com.example.springdbprac.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 memberRepositoryV0 = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException {
+        // save
+        Member member = new Member("memberV0", 10000);
+        memberRepositoryV0.save(member);
+
+        // findById
+        Member memberResult = memberRepositoryV0.findById(member.getMemberId());
+        assertThat(memberResult.getMemberId()).isEqualTo(member.getMemberId());
+
+        // update
+        memberRepositoryV0.update(member.getMemberId(), 20000);
+        Member updateMember = memberRepositoryV0.findById(member.getMemberId());
+        assertThat(updateMember).isNotEqualTo(member);
+
+        // delete
+        memberRepositoryV0.delete(member.getMemberId());
+        assertThatThrownBy(() -> memberRepositoryV0.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+}
