a1a7cddd0a27790f0ca7a07506bcbc34c271b558
chhong <chhong@donga.com>
2023-07-11 22:51:54 +0900

    07_11 kafka settings

---
+++ kafkaPrac/src/test/java/com/study/kafkaprac/KafkaPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.kafkaprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
