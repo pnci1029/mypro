66d2717d62c7f3c03f263bee0ee64629c0324a82
chhong <chhong@donga.com>
2023-10-28 19:57:31 +0900

    10.28 fix

---
+++ springDbPrac/src/test/java/com/example/springdbprac/study/controller/ArticleControllerTest2.java
@@ -0,0 +1,37 @@
+package com.example.springdbprac.study.controller;
+
+import com.example.springdbprac.study.domain.entity.Article;
+import com.example.springdbprac.study.domain.entity.ArticleStatus;
+import com.example.springdbprac.study.domain.repository.ArticleRepository;
+import com.example.springdbprac.study.service.ArticleService;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class ArticleControllerTest2 {
+    @Autowired
+    ArticleService articleService;
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @Test
+    @DisplayName("게시글 생성")
+    void createArticles() {
+        Article article1 = new Article(1L, "title", "contents", 0, ArticleStatus.Etc, null);
+        Article article2 = new Article(2L, "title", "contents", 0, ArticleStatus.Etc, null);
+        Article article3 = new Article(3L, "title", "contents", 0, ArticleStatus.Etc, null);
+
+        articleRepository.saveAll(List.of(article1, article2, article3));
+
+        List<Article> articles = articleRepository.findAll();
+        assertThat(articles).hasSize(3);
+    }
+}
