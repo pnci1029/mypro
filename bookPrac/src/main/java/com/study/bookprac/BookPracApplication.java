310d76d9ee68e4f84cb08f64dd99246b9f57909f
chhong <chhong@donga.com>
2023-11-11 14:00:52 +0900

    11.11 simple setup

---
+++ bookPrac/src/main/java/com/study/bookprac/BookPracApplication.java
@@ -0,0 +1,13 @@
+package com.study.bookprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookPracApplication.class, args);
+    }
+
+}
---
+++ bookPrac/src/test/java/com/study/bookprac/BookPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.bookprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
