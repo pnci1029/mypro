c4be1804f7dd604da398ca320b4dc791ca8a792d
chhong <chhong@donga.com>
2023-11-12 12:38:17 +0900

    11.12 article post basic

--- elasticSearchPrac/src/main/java/com/study/elasticsearchprac/controller/ArticleController.java
+++ elasticSearchPrac/src/main/java/com/study/elasticsearchprac/controller/ArticleController.java
@@ -5,25 +5,27 @@ import com.study.elasticsearchprac.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
-@RequestMapping("/rest")
-@RestController @RequiredArgsConstructor @Slf4j
-public class ArticleRestController {
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class ArticleController {
     private final ArticleService articleService;
 
     @PostMapping(value = "/article/create", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public String createArticle(@RequestPart(required = false) ArticleRequestDto articleRequestDto,
                                 @RequestPart(name = "multipartFiles", required = false) MultipartFile multipartFiles) {
         articleService.create(articleRequestDto, multipartFiles);
-        return "success";
+        return "/article/articleMain";
     }
 
-    @GetMapping("/get")
+    @GetMapping("/article")
     public String getArticle() {
-        return "1";
+        return "/article/articleMain";
     }
 }
