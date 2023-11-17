4123b59247a89632d561ae143a1a5abe0429bcd0
chhong <chhong@donga.com>
2023-11-17 21:33:53 +0900

    11.17 security prac

---
+++ security/src/main/java/com/example/security/mySecurity/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.example.security.mySecurity.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/api/**", corsConfiguration);
+        return new CorsFilter();
+    }
+}
