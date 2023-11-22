9e96e07cce05e304c361603698126581e8683b1f
chhong <chhong@donga.com>
2023-11-22 21:22:32 +0900

    11.22 security basic2

--- security/src/main/java/com/example/security/mySecurity/config/FilterConfig.java
+++ security/src/main/java/com/example/security/mySecurity/config/FilterConfig.java
@@ -1,6 +1,7 @@
 package com.example.security.mySecurity.config;
 
 import com.example.security.mySecurity.filter.MyFilter1;
+import com.example.security.mySecurity.filter.MyFilter2;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +12,20 @@ public class FilterConfig {
     @Bean
     public FilterRegistrationBean<MyFilter1> filterFirst() {
         FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
-        bean.addUrlPatterns("/**");
+        bean.addUrlPatterns("/*");
 
         // 우선 순위가 가장 높음
         bean.setOrder(0);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<MyFilter2> filterSecond() {
+        FilterRegistrationBean<MyFilter2> bean = new FilterRegistrationBean<>(new MyFilter2());
+        bean.addUrlPatterns("/*");
+
+        // 우선 순위
+        bean.setOrder(1);
         return bean;
     }
 }
--- security/src/main/java/com/example/security/mySecurity/config/SecurityConfig.java
+++ security/src/main/java/com/example/security/mySecurity/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.security.mySecurity.config;
 
+import com.example.security.mySecurity.filter.MyFilter1;
+import com.example.security.mySecurity.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,12 +20,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        // 필터 추가
+        // filter 1, 2, 3 중 가장 먼저 실행
+//        http.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class);
+        http.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class);
+
         http.csrf().disable();
         // 세션 x
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 // 필터를 통해 항상 corsFilter를 거쳐감
-                .addFilter(corsFilter)
+//                .addFilter(corsFilter)
 
                 // 폼 로그인이나 기본 http로그인 방식 x
                 .formLogin().disable()
