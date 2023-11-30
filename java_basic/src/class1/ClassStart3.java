aa8eaa73a350e0222bbf4fe0b0ded1426b4d719e
chhong <chhong@donga.com>
2023-11-30 21:49:36 +0900

    11.30 add java_basic

---
+++ java_basic/src/class1/ClassStart3.java
@@ -0,0 +1,15 @@
+package class1;
+
+public class ClassStart3 {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "학생1";
+        student1.age = 17;
+        student1.grade = 90;
+
+        Student student2 = new Student();
+        student2.name = "학생1";
+        student2.age = 17;
+        student2.grade = 90;
+    }
+}
