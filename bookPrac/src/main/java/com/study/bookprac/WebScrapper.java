0b3a84ed1a9fde487b8287055abec325d3fb8d46
chhong <chhong@donga.com>
2023-12-02 14:48:36 +0900

    12.02 new project setup

--- bookPrac/src/main/java/com/study/bookprac/WebScrapper.java
+++ bookPrac/src/main/java/com/study/bookprac/WebScrapper.java
@@ -1,8 +1,32 @@
 package com.study.bookprac;
 
-public class WebScrapper {
-    public static void main(String[] args) {
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
+@Slf4j
+public class WebScrapper {
+    public static void main(String[] args) throws IOException {
+        String url = "https://www.test.com/deal/rank";
+
+        Document document = Jsoup.connect(url).get();
+
+        Elements select = document.select(".deal-title");
+        System.out.println("select = " + select);
+        for (Element element : select) {
+            Elements value = select.select(".deal-name");
+            System.out.println("value = " + value);
+            System.out.println(element.select(".product-title").size());
+        }
+
+        Elements select2 = document.select("div.deal-title");
+        Elements select3 = document.select("product-link ");
+        System.out.println("select2 = " + select2.size());
+        System.out.println("select3 = " + select3.size());
 
     }
 }
