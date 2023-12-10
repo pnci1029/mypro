1e894b49a00fb7aef7f2c14f4417099acf4404b3
chhong <chhong@donga.com>
2023-12-10 15:26:38 +0900

    12.10 add staic

---
+++ java_basic/src/static2/Deco1.java
@@ -0,0 +1,8 @@
+package static2;
+
+public class Deco1 {
+    public String deco1(String value) {
+        return "*" + value + "*";
+    }
+
+}
---
+++ java_basic/src/static2/Deco2.java
@@ -0,0 +1,9 @@
+package static2;
+
+public class Deco2 {
+
+    public static String deco1(String value) {
+        return "*" + value + "*";
+    }
+
+}
