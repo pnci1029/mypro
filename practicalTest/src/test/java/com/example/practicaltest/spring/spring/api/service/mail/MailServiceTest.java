43803ec8727c1fbfa6d2fb0f5c129e8e8576bcc0
chhong <81909140+pnci1029@users.noreply.github.com>
2024-03-05 14:01:37 +0900

    24.03.05 test with mockito v2

--- practicalTest/src/test/java/com/example/practicaltest/spring/spring/api/service/mail/MailServiceTest.java
+++ practicalTest/src/test/java/com/example/practicaltest/spring/spring/api/service/mail/MailServiceTest.java
@@ -6,16 +6,24 @@ import com.example.practicaltest.spring.spring.domain.history.mail.MailSendHisto
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class MailServiceTest {
+
+    @Mock
+    MailSendClient mailSendClient;
+
+    @Mock
+    MailSendHistoryRepository mailSendHistoryRepository;
 
     @DisplayName("메일 전송 테스트")
     @Test
@@ -26,8 +34,8 @@ class MailServiceTest {
          * private final MailSendClient mailSendClient;
          * private final MailSendHistoryRepository mailSendHistoryRepository;
          */
-        MailSendClient mailSendClient = Mockito.mock(MailSendClient.class);
-        MailSendHistoryRepository mailSendHistoryRepository = Mockito.mock(MailSendHistoryRepository.class);
+//        MailSendClient mailSendClient = Mockito.mock(MailSendClient.class);
+//        MailSendHistoryRepository mailSendHistoryRepository = Mockito.mock(MailSendHistoryRepository.class);
 
         MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
 
