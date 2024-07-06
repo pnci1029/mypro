55662b91f7767c6b221b21a152c9dfcd0014c75e
chhong <pnci1029@gmail.com>
2024-07-06 12:31:10 +0900

    24.07.06 add wordCountHandler implementation

--- java_basic/src/udemy/ThroughputHttpMethod.java
+++ java_basic/src/udemy/ThroughputHttpMethod.java
@@ -1,8 +1,11 @@
 package udemy;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -25,5 +28,32 @@ public class ThroughputHttpMethod {
         ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
         server.setExecutor(executor);
         server.start();
+    }
+
+    private static class WordCountHandler implements HttpHandler {
+        private String text;
+
+        public WordCountHandler(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String query = exchange.getRequestURI().getQuery();
+            String[] keyValue = query.split("=");
+            String action = keyValue[0];
+            String word = keyValue[1];
+
+            if (!action.equals("word")) {
+                exchange.sendResponseHeaders(400, 0);
+                return;
+            }
+            long count = countWord(word);
+            byte[] response = Long.toString(count).getBytes();
+            exchange.sendResponseHeaders(200, response.length);
+            OutputStream responseBody = exchange.getResponseBody();
+            responseBody.write(response);
+            responseBody.close();
+        }
     }
 }
