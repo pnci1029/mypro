7868726be2256bd072bbe06b6106b4c26afc2b3f
chhong <pnci1029@gmail.com>
2024-07-08 20:36:25 +0900

    24.07.08 reason why thread result changes

--- java_basic/src/udemy/inventory/main.java
+++ java_basic/src/udemy/inventory/main.java
@@ -1,11 +1,56 @@
 package udemy.inventory;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         InventoryCounter inventoryCounter = new InventoryCounter();
 
         IncreamentThread increamentThread = new IncreamentThread(inventoryCounter);
         DecreamentThread decreamentThread = new DecreamentThread(inventoryCounter);
+
+        /**
+          10000 - 10000이기 때문에 0 도출
+         */
+        increamentThread.start();
+        increamentThread.join();
+
+        decreamentThread.start();
+        decreamentThread.join();
+        System.out.println("increamentThread = " + increamentThread);
+
+        /**
+         10000 - 10000이기 때문에 0이 예상되나
+         매번 결과가 달라짐 -165...
+         */
+        increamentThread.start();
+        decreamentThread.start();
+
+        increamentThread.join();
+        decreamentThread.join();
+        System.out.println("increamentThread = " + increamentThread);
+
+
+        /**
+         그 이유는 스레드간 리소스 공유 때문인데,
+
+         increament실제 작업은
+         1. 현재 counter에 값 할당
+         2. 새로운 값 counter + 1 할당
+         3. items = 새로운값 = counter + 1
+         순으로 이어진다.
+
+         그러나 위 3개가 순차적으로 진행되지않는 케이스가 발생한다.
+         ex)
+
+         increament                             decreament
+         1. current = 0
+         2. newVal = current +1
+                                                3. current = 0
+                                                4. newVal = current -1
+                                                5. items = newVal = -1
+         6. items = newVal = 1
+
+         OS 스레드의 스케줄링의 차이에 따라 이전 값이 정의되지않고 처리될수있음
+         */
     }
 
     // 재고 10000개 추가
