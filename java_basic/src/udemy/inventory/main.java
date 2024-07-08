45a0379a9e9c475537600ac8fe160cb2225f6b0b
chhong <pnci1029@gmail.com>
2024-07-08 20:26:52 +0900

    24.07.08 setup for inventory thread test

--- java_basic/src/udemy/inventory/main.java
+++ java_basic/src/udemy/inventory/main.java
@@ -2,7 +2,43 @@ package udemy.inventory;
 
 public class main {
     public static void main(String[] args) {
+        InventoryCounter inventoryCounter = new InventoryCounter();
 
+        IncreamentThread increamentThread = new IncreamentThread(inventoryCounter);
+        DecreamentThread decreamentThread = new DecreamentThread(inventoryCounter);
+    }
+
+    // 재고 10000개 추가
+    public static class IncreamentThread extends Thread{
+        private InventoryCounter inventoryCounter;
+
+        public IncreamentThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increase();
+            }
+        }
+    }
+
+    // 재고 10000개 감소
+    public static class DecreamentThread extends Thread{
+        private InventoryCounter inventoryCounter;
+
+
+        public DecreamentThread(InventoryCounter increamentThread) {
+            this.inventoryCounter = increamentThread;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrease();
+            }
+        }
     }
 
     private static class InventoryCounter{
