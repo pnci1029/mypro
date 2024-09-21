e0e9168fb679350065bb5887a5364b7ff1f6f211
chhong <pnci1029@gmail.com>
2024-09-21 14:34:08 +0900

    24.09.21 create default kafka producer factory config

--- coupon-system/api/src/main/java/com/example/api/config/KafkaProducerConfig.java
+++ coupon-system/api/src/main/java/com/example/api/config/KafkaProducerConfig.java
@@ -6,6 +6,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
@@ -15,12 +16,19 @@ import java.util.Map;
 public class KafkaProducerConfig {
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, Long> producerFactory() {
         Map<String, Object> config = new HashMap<>();
+
+        // 서버 정보 저장
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Long> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
