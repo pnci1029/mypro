5c6cc066a24cc70e199b61e32a8569be928262a8
chhong <pnci1029@gmail.com>
2024-09-21 14:38:22 +0900

    24.09.21 create apply logic v3

--- coupon-system/api/src/main/java/com/example/api/service/ApplyService.java
+++ coupon-system/api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.producer.CouponCreateProducer;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,23 @@ public class ApplyService {
 
     private final CouponRepository couponRepository;
     private final CouponCountRepository couponCountRepository;
+    private final CouponCreateProducer couponCreateProducer;
 
     public ApplyService(CouponRepository couponRepository,
-                        CouponCountRepository couponCountRepository) {
+                        CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
+    }
+
+    public void applyV3(Long userId) {
+        Long count = couponCountRepository.increment();
+
+        if (count > 100) {
+            return;
+        }
+
+        couponCreateProducer.create(userId);
     }
 
     public void applyV2(Long userId) {
