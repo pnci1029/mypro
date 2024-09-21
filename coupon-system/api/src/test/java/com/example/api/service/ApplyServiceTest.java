1eb3212bdfae20a31efe02250fd38e183594dfb6
chhong <pnci1029@gmail.com>
2024-09-21 15:20:12 +0900

    24.09.21 create apply logic v3 test

--- coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -35,9 +35,39 @@ class ApplyServiceTest {
     }
 
 
-    @DisplayName("1000개의 요청을 받았을때 100개까지 쿠폰을 등록한다.")
+    @DisplayName("V3.1000개의 요청을 받았을때 100개까지 쿠폰을 등록한다.")
     @Test
-    void checkWhenApplyMultipleRequest() throws InterruptedException {
+    void checkWhenApplyMultipleRequestV3() throws InterruptedException {
+        // given
+        int threadCount = 1000;
+
+        // 멀티스레드 병렬작업을 도와주는 자바 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // 다른 스레드에서 수행하는 작업을 기다리도록 처리하는 클래스
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        // when
+        for(int i = 0; i < threadCount; i++) {
+            int userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.applyV3((long) userId);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+        long count = couponRepository.count();
+
+        // then
+        assertThat(count).isEqualTo(100);
+    }
+
+    @DisplayName("V2.1000개의 요청을 받았을때 100개까지 쿠폰을 등록한다.")
+    @Test
+    void checkWhenApplyMultipleRequestV2() throws InterruptedException {
         // given
         int threadCount = 1000;
 
