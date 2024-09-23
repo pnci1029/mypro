53257440259fa8e03987b7a2c02d893470936f2d
chhong <pnci1029@gmail.com>
2024-09-23 20:37:13 +0900

    24.09.23 fix something

--- coupon-system/consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
+++ coupon-system/consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
@@ -1,14 +1,20 @@
 package com.example.consumer.consumer;
 
+import com.example.consumer.domain.Coupon;
+import com.example.consumer.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component @RequiredArgsConstructor
 public class CouponCreatedConsumer {
+
+    private final CouponRepository couponRepository;
 
     @KafkaListener(groupId = "group_1", topics = "coupon_create")
     public void listener(Long userId) {
         System.out.println(userId);
+        couponRepository.save(new Coupon(userId));
 
     }
 }
