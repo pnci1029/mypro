9299c7ce3badb624178b965266719aba089f91d7
chhong <pnci1029@gmail.com>
2024-09-23 20:16:34 +0900

    24.09.21 create consumer config

--- coupon-system/consumer/src/main/java/com/example/consumer/config/KafkaConsumerConfig.java
+++ coupon-system/consumer/src/main/java/com/example/consumer/config/KafkaConsumerConfig.java
@@ -1,6 +1,7 @@
 package com.example.consumer.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -22,7 +23,7 @@ public class KafkaConsumerConfig {
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_1");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
 
         return new DefaultKafkaConsumerFactory<>(config);
     }
