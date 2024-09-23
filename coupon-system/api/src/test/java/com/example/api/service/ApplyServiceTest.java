5bcbe38a62acca185db1c97e516d15bdede4ec1c
chhong <pnci1029@gmail.com>
2024-09-23 21:11:48 +0900

    24.09.23 add new version

--- coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -34,6 +34,37 @@ class ApplyServiceTest {
         assertThat(count).isEqualTo(1L);
     }
 
+    @DisplayName("V4.1명당 1개의 쿠폰을 발급하고 1000개의 요청을 받았을때 100개까지 쿠폰을 등록한다.")
+    @Test
+    void checkWhenApplyMultipleRequestV4() throws InterruptedException {
+        // given
+        int threadCount = 1000;
+
+        // 멀티스레드 병렬작업을 도와주는 자바 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // 다른 스레드에서 수행하는 작업을 기다리도록 처리하는 클래스
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        // when
+        for(int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.applyV4(1L);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+        Thread.sleep(2000);
+        long count = couponRepository.count();
+
+        // then
+        assertThat(count).isEqualTo(1);
+    }
+
 
     @DisplayName("V3.1000개의 요청을 받았을때 100개까지 쿠폰을 등록한다.")
     @Test
@@ -59,7 +90,9 @@ class ApplyServiceTest {
             });
         }
         countDownLatch.await();
+        Thread.sleep(2000);
         long count = couponRepository.count();
+
 
         // then
         assertThat(count).isEqualTo(100);
