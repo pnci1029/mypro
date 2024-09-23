b191b12ba7d93ffabd27e3ac353e18f27b1eecf1
chhong <pnci1029@gmail.com>
2024-09-23 21:11:34 +0900

    24.09.23 add repository function

--- coupon-system/api/src/main/java/com/example/api/repository/AppliedUserRepository.java
+++ coupon-system/api/src/main/java/com/example/api/repository/AppliedUserRepository.java
@@ -1,16 +1,19 @@
 package com.example.api.repository;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
 /**
  * 1인 1개 쿠폰 케이스에 유저 정보를 set으로 담을 레디스 레포
  */
-@Repository @RequiredArgsConstructor
+@Repository
 public class AppliedUserRepository {
 
     private final RedisTemplate<String, String> redisTemplate;
+
+    public AppliedUserRepository(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 
     public Long add(Long userId) {
         return redisTemplate.opsForSet()
