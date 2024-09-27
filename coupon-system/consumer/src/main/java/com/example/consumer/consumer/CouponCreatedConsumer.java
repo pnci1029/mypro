68937e200f8a9b437bd0aaa075a00112bac7991d
chhong <pnci1029@gmail.com>
2024-09-27 19:50:14 +0900

    24.09.27 add something

--- coupon-system/consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
+++ coupon-system/consumer/src/main/java/com/example/consumer/consumer/CouponCreatedConsumer.java
@@ -1,20 +1,32 @@
 package com.example.consumer.consumer;
 
 import com.example.consumer.domain.Coupon;
+import com.example.consumer.domain.FailedEvent;
 import com.example.consumer.repository.CouponRepository;
+import com.example.consumer.repository.FailedEventRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
 
 @Component @RequiredArgsConstructor
 public class CouponCreatedConsumer {
 
     private final CouponRepository couponRepository;
+    private final FailedEventRepository failedEventRepository;
+    private final Logger logger = LoggerFactory.getLogger(CouponCreatedConsumer.class);
 
     @KafkaListener(groupId = "group_1", topics = "coupon_create")
     public void listener(Long userId) {
-        System.out.println(userId);
-        couponRepository.save(new Coupon(userId));
+        try {
+            System.out.println(userId);
+            couponRepository.save(new Coupon(userId));
+        } catch (Exception e) {
+            logger.error("failed event : "+e.getMessage());
+            failedEventRepository.save(new FailedEvent(userId));
+        }
 
     }
 }
