372727b060854dfedae4ee865871dca4003d4858
chhong <pnci1029@gmail.com>
2024-10-02 21:26:16 +0900

    24.10.02 method extracting

--- readable-code/src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
+++ readable-code/src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
@@ -11,63 +11,18 @@ public class MinesweeperGame {
     private static int gameStatus = 0; // 0: 게임 중, 1: 승리, -1: 패배
 
     public static void main(String[] args) {
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.out.println("지뢰찾기 게임 시작!");
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        // 시작 알림 텍스트
+        showGameStartComments();
+
         Scanner scanner = new Scanner(System.in);
-        for (int row = 0; row < 8; row++) {
-            for (int column = 0; column < 10; column++) {
-                board[row][column] = "□";
-            }
-        }
-        for (int i = 0; i < 10; i++) {
-            int col = new Random().nextInt(10);
-            int row = new Random().nextInt(8);
-            landMines[row][col] = true;
-        }
-        for (int row = 0; row < 8; row++) {
-            for (int column = 0; column < 10; column++) {
-                int count = 0;
-                if (!landMines[row][column]) {
-                    if (row - 1 >= 0 && column - 1 >= 0 && landMines[row - 1][column - 1]) {
-                        count++;
-                    }
-                    if (row - 1 >= 0 && landMines[row - 1][column]) {
-                        count++;
-                    }
-                    if (row - 1 >= 0 && column +1 < 10 && landMines[row - 1][column +1]) {
-                        count++;
-                    }
-                    if (column -1 >= 0 && landMines[row][column -1]) {
-                        count++;
-                    }
-                    if (column +1 < 10 && landMines[row][column +1]) {
-                        count++;
-                    }
-                    if (row + 1 < 8 && column -1 >= 0 && landMines[row + 1][column -1]) {
-                        count++;
-                    }
-                    if (row + 1 < 8 && landMines[row + 1][column]) {
-                        count++;
-                    }
-                    if (row + 1 < 8 && column +1 < 10 && landMines[row + 1][column +1]) {
-                        count++;
-                    }
-                    landMineCounts[row][column] = count;
-                    continue;
-                }
-                landMineCounts[row][column] = 0;
-            }
-        }
+
+        // 게임 초기화
+        initializeGame();
+
         while (true) {
-            System.out.println("   a b c d e f g h i j");
-            for (int row = 0; row < 8; row++) {
-                System.out.printf("%d  ", row + 1);
-                for (int column = 0; column < 10; column++) {
-                    System.out.print(board[row][column] + " ");
-                }
-                System.out.println();
-            }
+            // 보드를 그리는 부분
+            showBoard();
+
             if (gameStatus == 1) {
                 System.out.println("지뢰를 모두 찾았습니다. GAME CLEAR!");
                 break;
@@ -76,7 +31,6 @@ public class MinesweeperGame {
                 System.out.println("지뢰를 밟았습니다. GAME OVER!");
                 break;
             }
-            System.out.println();
             System.out.println("선택할 좌표를 입력하세요. (예: a1)");
             String cellInput = scanner.nextLine();
             System.out.println("선택한 셀에 대한 행위를 선택하세요. (1: 오픈, 2: 깃발 꽂기)");
@@ -156,6 +110,71 @@ public class MinesweeperGame {
                 System.out.println("잘못된 번호를 선택하셨습니다.");
             }
         }
+    }
+
+    private static void showBoard() {
+        System.out.println("   a b c d e f g h i j");
+        for (int row = 0; row < 8; row++) {
+            System.out.printf("%d  ", row + 1);
+            for (int column = 0; column < 10; column++) {
+                System.out.print(board[row][column] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private static void initializeGame() {
+        for (int row = 0; row < 8; row++) {
+            for (int column = 0; column < 10; column++) {
+                board[row][column] = "□";
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            int col = new Random().nextInt(10);
+            int row = new Random().nextInt(8);
+            landMines[row][col] = true;
+        }
+        for (int row = 0; row < 8; row++) {
+            for (int column = 0; column < 10; column++) {
+                int count = 0;
+                if (!landMines[row][column]) {
+                    if (row - 1 >= 0 && column - 1 >= 0 && landMines[row - 1][column - 1]) {
+                        count++;
+                    }
+                    if (row - 1 >= 0 && landMines[row - 1][column]) {
+                        count++;
+                    }
+                    if (row - 1 >= 0 && column +1 < 10 && landMines[row - 1][column +1]) {
+                        count++;
+                    }
+                    if (column -1 >= 0 && landMines[row][column -1]) {
+                        count++;
+                    }
+                    if (column +1 < 10 && landMines[row][column +1]) {
+                        count++;
+                    }
+                    if (row + 1 < 8 && column -1 >= 0 && landMines[row + 1][column -1]) {
+                        count++;
+                    }
+                    if (row + 1 < 8 && landMines[row + 1][column]) {
+                        count++;
+                    }
+                    if (row + 1 < 8 && column +1 < 10 && landMines[row + 1][column +1]) {
+                        count++;
+                    }
+                    landMineCounts[row][column] = count;
+                    continue;
+                }
+                landMineCounts[row][column] = 0;
+            }
+        }
+    }
+
+    private static void showGameStartComments() {
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        System.out.println("지뢰찾기 게임 시작!");
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
     }
 
     private static void open(int row, int col) {
