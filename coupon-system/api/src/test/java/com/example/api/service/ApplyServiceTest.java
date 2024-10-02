22bd3393aab20d906ac09231594d28fb2721b707
chhong <pnci1029@gmail.com>
2024-10-02 21:22:17 +0900

    24.10.02 add default test case

--- coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ coupon-system/api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -127,4 +127,34 @@ class ApplyServiceTest {
         // then
         assertThat(count).isEqualTo(100);
     }
+
+    @DisplayName("V1.1000개의 요청을 받았을때 100개까지 쿠폰을 등록한다.")
+    @Test
+    void checkWhenApplyMultipleRequestV1() throws InterruptedException {
+        // given
+        int threadCount = 1000;
+
+        // 멀티스레드 병렬작업을 도와주는 자바 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // 다른 스레드에서 수행하는 작업을 기다리도록 처리하는 클래스
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        // when
+        for(int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.applyV1(userId);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+        long count = couponRepository.count();
+
+        // then
+        assertThat(count).isEqualTo(100);
+    }
 }
