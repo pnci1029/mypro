077459a4cd5d28d19fbe6ab9dffc698c40919563
chhong <pnci1029@gmail.com>
2024-11-23 13:12:44 +0900

    24.11.23 내장 톰켓 프로젝트용 매인 메서드 생성

---
+++ 스프링부트핵심원리/embed/src/main/java/hello/embedded/EmbeddedTomcatServletMain.java
@@ -0,0 +1,10 @@
+package hello.embedded;
+
+import org.apache.catalina.startup.Tomcat;
+
+public class EmbeddedTomcatServletMain {
+
+    public static void main(String[] args) {
+        System.out.println("EmbeddedTomcatServletMain.main");
+    }
+}
