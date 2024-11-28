66a0a7bfb05910d5f50d4521f3d8253295fe1351
chhong <pnci1029@gmail.com>
2024-11-28 14:23:36 +0900

    24.11.28 데이터소스 h2 추가

---
+++ 스프링부트핵심원리/autoconfig/src/main/java/hello/config/DbConfig.java
@@ -0,0 +1,26 @@
+package hello.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@Configuration
+public class DbConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        log.info("dataSource 빈 등록");
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setJdbcUrl("jdbc:h2:mem:test");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+}
