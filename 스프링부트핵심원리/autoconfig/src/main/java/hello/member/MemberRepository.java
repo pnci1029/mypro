f3d10fda17da1a2b71ed5161952b92bd6b21d6dd
chhong <pnci1029@gmail.com>
2024-11-28 14:33:29 +0900

    24.11.28 함수 생성

--- 스프링부트핵심원리/autoconfig/src/main/java/hello/member/MemberRepository.java
+++ 스프링부트핵심원리/autoconfig/src/main/java/hello/member/MemberRepository.java
@@ -1,5 +1,6 @@
 package hello.member;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,18 @@ public class MemberRepository {
 
     public MemberRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void initTable() {
+        jdbcTemplate.execute("create table member(member_id varchar primary key , name varchar)");
+    }
+
+    public void save(Member member) {
+        jdbcTemplate.update("insert into member(member_id, name) values( ?,? )", member.getMemberId(), member.getName());
+    }
+
+    public Member find(String memberId) {
+        return jdbcTemplate.queryForObject("select memberId, name from Member where member_id = ?",
+                new BeanPropertyRowMapper<>(Member.class), memberId);
     }
 }
