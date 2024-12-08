1541691141a18b1e8f9d571d62db6a7d5ea43aed
chhong <pnci1029@gmail.com>
2024-12-08 15:13:51 +0900

    24.12.08 @condition 빈 등록 추가

--- 스프링부트핵심원리/autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ 스프링부트핵심원리/autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,11 +1,14 @@
 package hello.config;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Conditional(MemoryCondition.class) // memoryCondition이 true이면 아래 bean 등록 / 아니면 미등록
 public class MemoryConfig {
 
     @Bean
