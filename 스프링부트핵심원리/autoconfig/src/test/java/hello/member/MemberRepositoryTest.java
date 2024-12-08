5367245db0b26ab7250a86c61dc99f2e697131e8
chhong <pnci1029@gmail.com>
2024-12-08 14:32:36 +0900

    24.12.08 테스트 추가

---
+++ 스프링부트핵심원리/autoconfig/src/test/java/hello/member/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package hello.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Transactional
+    @DisplayName("")
+    @Test
+    void memberTest() {
+        // given
+        Member member = new Member("idA", "memberA");
+        memberRepository.initTable();
+        memberRepository.save(member);
+
+        // when
+        Member resultMember = memberRepository.find(member.getMemberId());
+
+        // then
+        assertThat(resultMember.getMemberId()).isEqualTo(member.getMemberId());
+        assertThat(resultMember.getName()).isEqualTo(member.getName());
+    }
+}
