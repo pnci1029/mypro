4efeedbd0bacece4768d2e706807506d8ff53b31
chhong <pnci1029@gmail.com>
2024-12-14 14:12:36 +0900

    24.12.14 스프링부트 컨디셔널 어노테이션 수정

--- 스프링부트핵심원리/autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ 스프링부트핵심원리/autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -3,12 +3,15 @@ package hello.config;
 import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Conditional(MemoryCondition.class) // memoryCondition이 true이면 아래 bean 등록 / 아니면 미등록
+//@Conditional(MemoryCondition.class) // memoryCondition이 true이면 아래 bean 등록 / 아니면 미등록
+// 위 아래 어노테이션은 같음
+@ConditionalOnProperty(name = "memory", havingValue = "on")
 public class MemoryConfig {
 
     @Bean
