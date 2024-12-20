d076b99ad9227d2a41becf09997791bd010890dd
chhong <pnci1029@gmail.com>
2024-12-20 13:15:53 +0900

    24.12.20 커맨드라인 예제 항목 추가

---
+++ 스프링부트핵심원리/external/src/test/java/hello/external/CommandLineV1.java
@@ -0,0 +1,16 @@
+package hello.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CommandLineV1 {
+    public static void main(String[] args) {
+        /**
+         * edit configuration -> program argument -> dataA dataB
+         * //띄어쓰기 기준으로 구분됨
+         */
+        for (String arg : args) {
+            log.info("args:{}", arg);
+        }
+    }
+}
