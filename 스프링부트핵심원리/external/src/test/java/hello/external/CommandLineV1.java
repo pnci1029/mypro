676e7cad71da6a00d46bb393b5880884742e969e
chhong <pnci1029@gmail.com>
2024-12-20 13:31:01 +0900

    24.12.20 커맨드라인2 예제 항목 추가

--- 스프링부트핵심원리/external/src/test/java/hello/external/CommandLineV1.java
+++ 스프링부트핵심원리/external/src/test/java/hello/external/CommandLineV1.java
@@ -7,7 +7,7 @@ public class CommandLineV1 {
     public static void main(String[] args) {
         /**
          * edit configuration -> program argument -> dataA dataB
-         * url=devdb username=dev_user password=dev_pw
+         * url=devdb username=dev_user password=dev_pw mode=on
          * //띄어쓰기 기준으로 구분됨
          */
         for (String arg : args) {
--- 스프링부트핵심원리/external/src/test/java/hello/external/CommandLineV2.java
+++ 스프링부트핵심원리/external/src/test/java/hello/external/CommandLineV2.java
@@ -4,10 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.DefaultApplicationArguments;
 
+import java.util.List;
+import java.util.Set;
+
 @Slf4j
 public class CommandLineV2 {
     /**
      * 기존 커맨드 라인을 key-value 형태로 받을 수 있는
+     * --url=devdb --username=dev_user --password=dev_pw mode=on
      * 스프링의 표준 방식 활용
      */
     public static void main(String[] args) {
@@ -16,9 +20,16 @@ public class CommandLineV2 {
             /**
                  -> args = url=devdb
                  -> args = username=dev_user
-                 -> args =password=dev_pw
+                 -> args = password=dev_pw
+                 -> args = mode=on
              */
 
         }
+        ApplicationArguments appArgs = new DefaultApplicationArguments(args);
+
+        log.info("SourceArgs = {}", List.of(appArgs.getSourceArgs())); // SourceArgs = [url=devdb, username=dev_user, password=dev_pw, mode=on]
+        log.info("NonOptionArgs = {}", appArgs.getNonOptionArgs()); // NonOptionArgs = [mode=on]
+        log.info("OptionNames = {}", appArgs.getOptionNames()); // OptionNames = [password, url, username]
+
     }
 }
