3021a71f786c0197b568e1050f196250cd72ce95
chhong <pnci1029@gmail.com>
2024-12-20 13:24:04 +0900

    24.12.20 커맨드라인2 예제 항목 추가

---
+++ 스프링부트핵심원리/external/src/test/java/hello/external/CommandLineV2.java
@@ -0,0 +1,14 @@
+package hello.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CommandLineV2 {
+    /**
+     * 기존 커맨드 라인을 key-value 형태로 받을 수 있는
+     * 스프링의 표준 방식 활용
+     */
+    public static void main(String[] args) {
+
+    }
+}
