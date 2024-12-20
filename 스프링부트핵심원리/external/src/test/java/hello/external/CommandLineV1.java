c126666f9fd6ddd54558755d215cf6a8ccbbb938
chhong <pnci1029@gmail.com>
2024-12-20 13:21:48 +0900

    24.12.20 커맨드라인 예제 항목 추가

--- 스프링부트핵심원리/external/src/test/java/hello/external/CommandLineV1.java
+++ 스프링부트핵심원리/external/src/test/java/hello/external/CommandLineV1.java
@@ -7,6 +7,7 @@ public class CommandLineV1 {
     public static void main(String[] args) {
         /**
          * edit configuration -> program argument -> dataA dataB
+         * url=devdb username=dev_user password=dev_pw
          * //띄어쓰기 기준으로 구분됨
          */
         for (String arg : args) {
