a9b7b7b0e29a97d6045d655dffa4b5e319f12b9c
chhong <pnci1029@gmail.com>
2025-01-02 22:06:43 +0900

    25.1.2 커맨드라인 빈1

--- 스프링부트핵심원리/external/src/main/java/hello/CommandLineBean.java
+++ 스프링부트핵심원리/external/src/main/java/hello/CommandLineBean.java
@@ -1,8 +1,12 @@
 package hello;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @ComponentScan
@@ -13,4 +17,13 @@ public class CommandLineBean {
         this.arguments = arguments;
     }
 
+    @PostConstruct
+    public void init() {
+        log.info("source {}", List.of(arguments.getSourceArgs()));
+        log.info("options {}", arguments.getOptionNames());
+        Set<String> optionNames = arguments.getOptionNames();
+        for (String optionName : optionNames) {
+            log.info("option {}", optionName);
+        }
+    }
 }
