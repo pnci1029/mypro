e231b3a94e22122d36e513fe3457861342017350
chhong <pnci1029@gmail.com>
2025-02-02 15:24:13 +0900

    25. 2. 2 db 데이터 테스트

--- jooq-1/src/test/java/com/example/jooq1/Jooq1ApplicationTests.java
+++ jooq-1/src/test/java/com/example/jooq1/Jooq1ApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.jooq1;
 
+import org.jooq.DSLContext;
+import org.jooq.generated.tables.JServers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Jooq1ApplicationTests {
 
+    @Autowired
+    DSLContext dslContext;
+
     @Test
     void contextLoads() {
+        dslContext.selectFrom(JAc.SERVERS)
+                .limit(10)
+                .fetch();
     }
 
 }
