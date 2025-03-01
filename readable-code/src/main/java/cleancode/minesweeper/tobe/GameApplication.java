d692d2af55461488333a79f329bc7083ffb9a861
chhong <pnci1029@gmail.com>
2025-03-01 22:12:07 +0900

    25. 3 .01 변수명 리팩토링

--- readable-code/src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ readable-code/src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,10 +1,6 @@
 package cleancode.minesweeper.tobe;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
-
-public class MinesweeperGame {
+public class GameApplication {
 
 
     public static void main(String[] args) {
